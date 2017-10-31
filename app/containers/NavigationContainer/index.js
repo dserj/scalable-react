3e08d1b945313f26328728cb414c55e6ec0ba6cf
serj <>
2017-10-31 14:29:05 +0300

    added navigation container and conponent

---
+++ app/containers/NavigationContainer/index.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * NavigationContainer
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectNavigationContainer from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export class NavigationContainer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+NavigationContainer.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  navigationcontainer: makeSelectNavigationContainer(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'navigationContainer', reducer });
+const withSaga = injectSaga({ key: 'navigationContainer', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(NavigationContainer);
