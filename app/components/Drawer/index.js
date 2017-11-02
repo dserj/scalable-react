4f4468dceb28b5047054ada052bd714fa58b1167
serj <>
2017-11-02 15:49:03 +0300

    drawer styles

--- app/components/Drawer/index.js
+++ app/components/Drawer/index.js
@@ -7,11 +7,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './styles.css';
 
-const Wrapper = styled.div``;
-const Item = styled.div``;
+const Item = styled.div`
+  padding: 25px;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-bottom: solid 1px #f6f6f6;
+  
+  &:hover {
+    background-color: #f6f6f6;
+  }
+`;
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: -300px;
+  height: 100%;
+  width: 300px;
+  background: #fff;
+  padding: 0;
+  transition: .75s ease-in-out;
+  box-shadow: rgba(0, 0, 0, 0.15) 0 3px 10px, rgba(0, 0, 0, 0.22) 0 3px 10px;
+  z-index: 1000;
+`;
 
-function Drawer({ items, selectItem, itemLabelAttr, itemKeyAttr }) {
+function Drawer({ items, selectItem, itemLabelAttr, itemKeyAttr, isDrawerOpen }) {
   const itemNodes = items.map((item) => (
     <Item
       key={item[itemKeyAttr]}
@@ -21,7 +43,7 @@ function Drawer({ items, selectItem, itemLabelAttr, itemKeyAttr }) {
     </Item>
   ));
   return (
-    <Wrapper>
+    <Wrapper className={classNames('', { drawerOpen: isDrawerOpen })}>
       {itemNodes}
     </Wrapper>
   );
@@ -32,6 +54,7 @@ Drawer.propTypes = {
   selectItem: PropTypes.func.isRequired,
   itemLabelAttr: PropTypes.string.isRequired,
   itemKeyAttr: PropTypes.string.isRequired,
+  isDrawerOpen: PropTypes.bool.isRequired,
 };
 
 export default Drawer;
