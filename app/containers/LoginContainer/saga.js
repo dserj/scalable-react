333fa57bdd6263b281bef45d7477ee5010fbaa8f
serj <>
2017-11-16 11:22:12 +0300

    Login component: login go-back action

--- app/containers/LoginContainer/saga.js
+++ app/containers/LoginContainer/saga.js
@@ -1,6 +1,16 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
+import { goBack } from 'react-router-redux';
+import * as actions from './constants';
 
 // Individual exports for testing
-export default function* defaultSaga() {
-  // See example in containers/HomePage/saga.js
+
+export function* handleLogin() {
+  yield put(goBack());
+}
+
+export default function* rootSaga() {
+  yield [
+    yield takeLatest(actions.LOGIN, handleLogin),
+  ];
 }
