78727baa293a82f3a15940ed17f414a2f3267f65
serj <>
2017-11-17 18:10:23 +0300

    Add link feature: display it immediately

--- app/containers/LinkFormContainer/actions.js
+++ app/containers/LinkFormContainer/actions.js
@@ -12,3 +12,18 @@ export function addLink(link) {
     link,
   };
 }
+
+export function addLinkSuccess(link) {
+  return {
+    type: actions.ADD_LINK_SUCCESS,
+    link,
+  };
+}
+
+export function addLinkFailed(link, message) {
+  return {
+    type: actions.ADD_LINK_FAILED,
+    link,
+    message,
+  };
+}
--- app/containers/LinkFormContainer/saga.js
+++ app/containers/LinkFormContainer/saga.js
@@ -1,13 +1,17 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { takeLatest, call } from 'redux-saga/effects';
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { goBack } from 'react-router-redux';
 import * as actions from './constants';
 import { createLink } from '../../api';
+import { addLinkFailed, addLinkSuccess } from './actions';
 
 export function* addLink(action) {
   try {
-    yield call(createLink, action.link);
+    const serverLink = yield call(createLink, action.link);
+    yield put(addLinkSuccess(serverLink));
+    yield put(goBack());
   } catch (e) {
-    console.error(e);
+    yield put(addLinkFailed(action.link, e.message));
   }
 }
 
